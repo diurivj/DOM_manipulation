607bc7d6fe55124acdd621fb6eb0e9814cfa6964
diurivj <diurivj@icloud.com>
2019-01-18 17:14:49 -0600

    ahí esta amiguitos :fire: :octocat:

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let button = document.getElementById('change-color')
 let title = document.getElementsByTagName('h1')
+
 let buttonAddTodo = document.getElementById('addTodo')
 let todo = document.getElementsByName('todo')
 let list = document.getElementById('list')
+
+let inputURL = document.getElementsByName('url')
+let buttonURL = document.getElementById('addLink')
+let websites = document.getElementById('content')
 
 function changeColor() {
   if (title[0].style.color === 'blue') {
@@ -23,5 +28,22 @@ function addTodo() {
   todo[0].value = ''
 }
 
+
+function addULRToWebsite() {
+  let url_link = inputURL[0].value
+  let anchor = document.createElement('a')
+  anchor.href = 'https://' + url_link
+  anchor.target = '_blank'
+  anchor.innerHTML = '<span>😏 Go to the page</span>'
+  websites.appendChild(anchor)
+  inputURL[0].value = ''
+  console.log(inputURL)
+}
+
+list.addEventListener('click', e => {
+  list.removeChild(e.target)
+})
+
 button.onclick = changeColor
 buttonAddTodo.onclick = addTodo
+buttonURL.onclick = addULRToWebsite
